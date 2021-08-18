ed68ff32df78b8b9309b0b47f8e3ee8441a23ab1
charlesmugambi <charlesmugambi100@gmail.com>
2021-08-18 14:20:59 +0300

    responsive version

--- repo/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/moringa/Desktop/django-week-3ip/repo/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/charlesmugambi/Desktop/charles-repo/virtual/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
